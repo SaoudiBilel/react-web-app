90a2996392d2621895574cfe5b8ba2e0e6825e3d
Saoudi Bilel <bilel.seoudi@gmail.com>
2022-06-14 14:26:05 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Counter from './components/counter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-3">
+      <Counter title="Profile" value={1} image = "images/profile.png"/>
+      <Counter title="Panda" value={1} image = "images/panda.png"/>
     </div>
   );
 }
---
+++ src/components/counter.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function Counter(props) {
+  const [state, setState] = useState({ counter: 1, list: [0] });
+
+  const increment = () => {
+    let c = state.counter + 1;
+    setState({ ...state, counter: c, list: new Array(c).fill(0) });
+  };
+
+  const decrement = () => {
+    let c = state.counter - 1;
+    setState({ ...state, counter: c, list: new Array(c).fill(0) });
+  };
+
+  return (
+    <div className="card m-3">
+      <div className="card-header">
+        <strong>
+          {props.title} : {state.counter}
+        </strong>
+      </div>
+      <div>
+        <button className="btn btn-primary m-2" onClick={increment}>
+          +
+        </button>
+        <button className="btn btn-primary m-2" onClick={decrement}>
+          -
+        </button>
+      </div>
+      <div className="card-body">
+        {state.list.map((v, i) => (
+          <img key={i} width={100} src={props.image}></img>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
